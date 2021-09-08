c4410936664be7c68bf53075bbf3c1a52ab4fb41
Pedro Vitor <pedrovitorti@gmail.com>
2021-09-08 13:32:03 -0300

    Subindo arquivos do projeto para o github

---
+++ src/TestBackEndApi.Api/Controllers/CepController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestBackEndApi.Domain.Queries.Cep.Get;
+
+namespace TestBackEndApi.Api.Controllers
+{
+    [Route("api/cep")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CepController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        private async Task<IActionResult> Get([FromQuery] GetCepQuery query)
+        {
+            var response = await _mediator.Send(query);
+            if (response == null) return NotFound();
+            if (string.IsNullOrEmpty(response.Cep)) return BadRequest(response);
+            return Ok(response);
+        }
+
+    }
+}
